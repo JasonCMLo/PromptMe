8afacef824788cebdcf154f7e83bafa373a4f9c8
JasonCMLo <jason.chun.man.lo@gmail.com>
2023-04-26 13:24:49 -0400

    initial commit

---
+++ Backend/Controller/controller.story.js
@@ -0,0 +1,62 @@
+import Story from "../Models/models.story.js";
+import Prompt from "../Models/models.prompts.js";
+
+
+export function updateStory(req, res) {
+    Story.findByIdAndUpdate(req.body.id, {Story: req.body.Story}, {new: true}, (err, doc) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(doc);
+      }
+    });
+  };
+
+export function createPrompts(req, res) {
+
+    const {newPrompt} = (req.body);
+
+    console.log(newPrompt);
+
+    let mongoPrompt = new Prompt({
+        prompt: newPrompt
+    })
+
+    mongoPrompt.save();
+
+    res.send("Success!");
+};
+
+export function getPrompts(req, res) {
+    Prompt.find({}).then((items) => res.json(items));
+}
+
+export function getStories(req, res) {
+
+    let skip = req.query.skip || 0;
+    Story.find({}, undefined, {skip, limit: 5}).then((items) => res.json(items));
+}
+
+export function lockPrompt(req, res) {
+    
+    Prompt.find().then((items) => {
+        let x = Math.floor(Math.random() * items.length);
+        res.json(items[x].prompt)
+    })
+
+}
+
+export function createStory(req, res) {
+
+  const {prompt, story} = (req.body);
+
+  let newStory = new Story({
+    prompt: prompt,
+    story: story
+  })
+
+  newStory.save();
+
+  res.send("Success!");
+
+}
